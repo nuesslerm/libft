ecab9da56c6cc18a54699828179a134b154c1906
nuesslerm <markus@choco.com>
2025-05-15 00:13:58 +0200

    Add new linked list functions: ft_lstadd_back, ft_lstadd_front, ft_lstclear, ft_lstdelone, ft_lstiter, ft_lstlast, ft_lstmap, ft_lstnew, and ft_lstsize. Implementations include memory management and list manipulation logic. Update libft.h with new function prototypes.

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*current;
+	t_list	*next_node;
+
+	if (!lst || !del)
+		return;
+	current = *lst;
+	while (current)
+	{
+		next_node = current->next;
+		ft_lstdelone(current, del);
+		current = next_node;
+	}
+	*lst = NULL;
+}
